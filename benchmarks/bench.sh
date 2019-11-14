5183071edd0be0c29b633b29c8661e279b3c6eee
Kris <kris@jusiak.net>
2019-11-14 14:01:08 -0600

    update ut results

--- benchmarks/bench.sh
+++ benchmarks/bench.sh
@@ -5,3 +5,7 @@ CXX=clang++ CXXFLAGS="-std=c++2a -O3 -DNDEBUG --gcc-toolchain=/usr/lib/gcc9/9.2.
 CXX=g++ CXXFLAGS="-std=c++2a" make &> gcc9.bench
 CXX=g++ CXXFLAGS="-g -std=c++2a" make &> gcc9.debug.bench
 CXX=g++ CXXFLAGS="-std=c++2a -O3 -DNDEBUG" make &> gcc9.release.bench
+
+CXX=clang++ CXXFLAGS="-std=c++2a -stdlib=libc++" make ut &> ut.clang9.bench
+CXX=clang++ CXXFLAGS="-g -std=c++2a -stdlib=libc++" make ut &> ut.clang9.debug.bench
+CXX=clang++ CXXFLAGS="-std=c++2a -O3 -DNDEBUG -stdlib=libc++" make ut &> ut.clang9.release.bench
--- results/gnulplot.sh
+++ results/gnulplot.sh
@@ -53,6 +53,42 @@ function plot {
 EOF
 }
 
+function plot_ut {
+  echo "Framework clang9 clang9.debug clang9.release" > /tmp/file
+  echo -n "ut.header " >> /tmp/file
+  grep "^$1:" ut.clang9.bench | cut -d' ' -f2 | xargs -n3 | cut -d' ' -f$4 | head -1 | tail -1 | xargs echo -n >> /tmp/file
+  grep "^$1:" ut.clang9.debug.bench | cut -d' ' -f2 | xargs -n3 | cut -d' ' -f$4 | head -1 | tail -1 | xargs echo -n '' >> /tmp/file
+  grep "^$1:" ut.clang9.release.bench | cut -d' ' -f2 | xargs -n3 | cut -d' ' -f$4 | head -1 | tail -1 | xargs echo  -n '' >> /tmp/file
+
+  echo -n "ut.pch " >> /tmp/file
+  grep "^$1:" ut.clang9.bench | cut -d' ' -f2 | xargs -n3 | cut -d' ' -f$4 | head -2 | tail -1 | xargs echo -n >> /tmp/file
+  grep "^$1:" ut.clang9.debug.bench | cut -d' ' -f2 | xargs -n3 | cut -d' ' -f$4 | head -2 | tail -1 | xargs echo -n '' >> /tmp/file
+  grep "^$1:" ut.clang9.release.bench | cut -d' ' -f2 | xargs -n3 | cut -d' ' -f$4 | head -2 | tail -1 | xargs echo  -n '' >> /tmp/file
+
+  echo -n "ut.module " >> /tmp/file
+  grep "^$1:" ut.clang9.bench | cut -d' ' -f2 | xargs -n3 | cut -d' ' -f$4 | head -3 | tail -1 | xargs echo -n >> /tmp/file
+  grep "^$1:" ut.clang9.debug.bench | cut -d' ' -f2 | xargs -n3 | cut -d' ' -f$4 | head -3 | tail -1 | xargs echo -n '' >> /tmp/file
+  grep "^$1:" ut.clang9.release.bench | cut -d' ' -f2 | xargs -n3 | cut -d' ' -f$4 | head -3 | tail -1 | xargs echo  -n '' >> /tmp/file
+
+  gnuplot << EOF
+    set tmargin 5
+    set title '$3 / $1 (smaller is better)'
+    set title font ",16"
+    set ylabel "$2"
+    set terminal png
+    set output 'ut_$3_$1.png'
+    set style histogram clustered gap 1
+    set grid y
+    set boxwidth 1.0 absolute
+    set style fill transparent solid 1.0 noborder
+    set style data histogram
+    set xtics rotate by 45 right
+    set xrange [0.3:4.7]
+    set offsets 0, 0, 0.01, 0
+    plot "/tmp/file" using 2:xticlabels(1) title 'clang9', '' u 3 title 'clang9.debug', '' u 4 title 'clang9.release'
+EOF
+}
+
 rm -f *.png
 
 plot include "time [s]" Compilation 1
@@ -71,3 +107,6 @@ plot suite+assert "time [s]" Execution 2
 
 plot suite+assert+stl "time [s]" Compilation 1
 plot suite+assert+stl "time [s]" Execution 2
+
+plot_ut suite+assert+stl "time [s]" Compilation 1
+plot_ut suite+assert+stl "time [s]" Execution 2
