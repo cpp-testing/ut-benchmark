32c71dd2357a3c0f8762e5baea599b1d051213cc
Kris <kris@jusiak.net>
2019-11-24 10:04:42 -0600

    boost.test

---
+++ benchmarks/boost.test/assert.cpp
@@ -0,0 +1,7 @@
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(assert) {
+	for (auto i = 0; i < 1'000'000; ++i) {
+		BOOST_TEST(i == i);
+	}
+}
--- benchmarks/boost/include.cpp
+++
@@ -1,2 +0,0 @@
-#define BOOST_TEST_MODULE IncludeModule
-#include <boost/test/unit_test.hpp>
